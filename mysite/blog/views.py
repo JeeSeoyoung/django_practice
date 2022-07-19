dab6ab74f1c9c839fcdfcb45655e3d58bd61eca7
JeeSeoyoung <jiseo09863@gmail.com>
2022-07-19 17:22:45 +0900

    template에 post_list전달

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -11,4 +11,4 @@ def post_list(req:HttpRequest)-> HttpResponse:
         {'id':5, 'title':'django', 'content':'django is ...'},
     ]
     
-    return HttpResponse(posts)
+    return render(req, 'blog/post_list.html', {'post_list':posts})
