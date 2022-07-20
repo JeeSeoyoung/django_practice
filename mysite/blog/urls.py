2da9e76bc4f426d78b764b27e9bb64c60a141392
JeeSeoyoung <jiseo09863@gmail.com>
2022-07-20 14:24:29 +0900

    blog완성

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,7 +1,14 @@
-import imp
+from telnetlib import DET
 from django.urls import path
-from . import views
+from django.views.generic import ListView, DetailView
+# from . import views
+from . import models
+
+app_name = 'blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    # path('', views.post_list, name='post_list'),
+    # path('<int:pk>/', views.post_detail, name='post_detail'),
+    path('', ListView.as_view(model=models.Post), name='post_list'),
+    path('<int:pk>/', DetailView.as_view(model=models.Post), name='post_detail'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,13 @@
 from django.http import HttpRequest,HttpResponse
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 def post_list(req:HttpRequest)-> HttpResponse:
-    posts = [
-        {'id':1, 'title':'html', 'content':'html is ...'},
-        {'id':2, 'title':'cdd', 'content':'cdd is ...'},
-        {'id':3, 'title':'javascript', 'content':'javascript is ...'},
-        {'id':4, 'title':'python', 'content':'python is ...'},
-        {'id':5, 'title':'django', 'content':'django is ...'},
-    ]
-    
+    posts = models.Post.objects.all()
     return render(req, 'blog/post_list.html', {'post_list':posts})
+
+def  post_detail(req: HttpRequest, pk: int) -> HttpResponse:
+    post = models.Post.objects.get(pk=pk)
+    return render(req, 'blog/post_detail.html', {'post':post})
+    
